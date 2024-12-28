6fbfc8c067cac135761edec3e124348163e95be4
Pano <panosevag@gmail.com>
2024-12-28 20:19:29 +0200

    created engine

--- HotelAPI/app.py
+++ HotelAPI/app.py
@@ -1,22 +1,21 @@
-from dataclasses import dataclass
-
 import uvicorn
 from fastapi import FastAPI
 
+from hotel.db.engine import init_db
+
 app = FastAPI()
 
+DB_FILE = "sqlite:///hotel.db"
+
+
+@app.on_event("startup")
+def startup():
+    init_db(DB_FILE)
 
 
 @app.get("/")
 def read_root():
     return "The server is running"
-
-
-@app.get("/customers")
-def read_customers():
-    return [
-        Customer(id=1, first_name="John", last_name="Doe", email="john.doe@example.com")
-    ]
 
 
 if __name__ == "__main__":
