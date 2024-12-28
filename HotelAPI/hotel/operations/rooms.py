ec305a14d04d1801d3a1f715864e7334c583125f
Pano <panosevag@gmail.com>
2024-12-28 22:39:42 +0200

    setup routerr for room queries

--- HotelAPI/hotel/operations/rooms.py
+++ HotelAPI/hotel/operations/rooms.py
@@ -4,3 +4,8 @@ from hotel.db.models import DBRoom
 
 def read_all_rooms():
     return DBSession().query(DBRoom).all()
+
+
+def read_room(room_id: int):
+    # return DBSession().query(DBRoom).filter(DBRoom.id == room_id).first()
+    return DBSession().query(DBRoom).get(room_id)
